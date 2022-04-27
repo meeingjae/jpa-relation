53cfbbe5b79221a1ffa1729d6022e7cca2b935e8
Minjae Kim <minjae.kim@mytrum.com>
2022-04-27 21:21:24 +0900

    feat: 사람, 자동차 Jpa Repository 생성

--- src/main/java/com/jparelation/entity/Person.java
+++ src/main/java/com/jparelation/entity/Person.java
@@ -1,25 +1,36 @@
 package com.jparelation.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
 import java.util.List;
 
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Person {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long personId;
 
     @Column
     private String name;
 
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "person")
-    @JoinColumn(name = "personId")
     private List<Car> cars;
+
+    @Override
+    public String toString() {
+        return "personId : " + this.personId + " name : " + this.name;
+    }
 }
